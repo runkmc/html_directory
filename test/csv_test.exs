7ab58258fd9d9ad62c258b771d8bd6d3a591e9d6
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-08-29 22:47:35 -0700

    Add additional tests to cover different header formats

--- test/csv_test.exs
+++ test/csv_test.exs
@@ -2,9 +2,10 @@ defmodule CSVTest do
   use ExUnit.Case
 
   test "headers are properly created" do
-    headers = Directory.CSV.create_headers "HouseholdID,Family Name,Street
-    (Permanent Address),City (Permanent Address),State (Permanent Address),Zip"
+    headers = Directory.CSV.create_headers "HouseholdID,Family Name,Street (Permanent Address),City (Permanent Address),State (Permanent Address),Zip"
     assert Enum.member?(headers, :householdid)
+    assert Enum.member?(headers, :family_name)
+    assert Enum.member?(headers, :street_permanent_address)
   end
 
 end
