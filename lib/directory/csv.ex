f757359a673c024783e63b09b153ee875af4c53b
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-08-30 11:31:47 -0700

    CSV Parser correctly parses CSV files

--- lib/directory/csv.ex
+++ lib/directory/csv.ex
@@ -1,21 +1,30 @@
 defmodule Directory.CSV do
 
   @moduledoc """
-  Turn a CSV file into a parsable format
+  Turn a CSV file into a list of maps
   """
 
   def parse(filename) do
     file = File.open!(filename)
     headers = create_headers(IO.read(file, :line))
+    Enum.map(IO.stream(file, :line), &create_line(&1, headers))
   end
 
   def create_headers(line_of_headers) do
     line_of_headers
-      |> String.strip
+      |> String.trim
       |> String.replace(" ", "_")
       |> String.replace(~r{[\(\)]}, "")
       |> String.downcase
       |> csv_data(&String.to_atom/1)
+  end
+
+  def create_line(line, headers) do
+    data = line 
+      |> String.trim
+      |> String.split(~r/,(?=(?:[^"]*"[^"]*")*[^"]*$)/)
+    Enum.zip(headers, data) 
+      |> Enum.into(%{})
   end
 
   defp csv_data(line, fun) do
--- test/csv_test.exs
+++ test/csv_test.exs
@@ -2,10 +2,23 @@ defmodule CSVTest do
   use ExUnit.Case
 
   test "headers are properly created" do
-    headers = Directory.CSV.create_headers "HouseholdID,Family Name,Street (Permanent Address),City (Permanent Address),State (Permanent Address),Zip"
+    file = File.open!("./test/test.csv")
+    headers = Directory.CSV.create_headers(IO.read(file, :line))
     assert Enum.member?(headers, :householdid)
     assert Enum.member?(headers, :family_name)
     assert Enum.member?(headers, :street_permanent_address)
   end
 
+  test "data is a list of maps" do
+    data = Directory.CSV.parse("./test/test.csv")
+    assert is_list(data)
+    assert is_map(List.first data)
+  end
+ 
+  test "data is matched with correct headers" do
+    data = Directory.CSV.parse("./test/test.csv")
+    assert (List.first data)[:family_name] == "Sisko"
+    assert (List.first data)[:members] == "\"Jake, Benjamin\""
+    assert (List.first data)[:phone_numbers] == "\"\""
+  end
 end
