86d6a2db5fea398a6c85fca3f5381a5b4867a6db
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-10-20 13:16:17 -0700

    Strip quotes from fields

--- lib/directory.ex
+++ lib/directory.ex
@@ -1,8 +1,18 @@
 defmodule Directory do
 
+  def main(args) do
+    {csv, html} = Directory.CLI.run(args)
+    members = Directory.CSV.parse(csv)
+    generate(html, members)
+  end
+
   def generate(filename, data) do
-    file = File.open(filename, [:write, :exclusive])
+    {_,file} = File.open(filename, [:write, :exclusive])
     finish(file, data)
+  end
+
+  def strip_quotes(string) do
+    String.replace(string, "\"", "")
   end
 
   def finish({:error, _}, _) do
@@ -15,11 +25,11 @@ defmodule Directory do
       IO.puts file, """
       <div class="directory-listing">
         <h1>#{listing[:addressee]}</h1>
-        <p class="members">#{listing[:members]}</p>
-        <span class="member-address"><p>#{listing[:street_permanent_address]}</p>
-        <p>#{listing[:city_permanent_address]}, #{listing[:state_permanent_address]} ##{listing[:zip_permanent_address]}</p></span>
+        <p class="members">#{strip_quotes(listing[:members])}</p>
+        <div class="member-address"><p>#{listing[:street_permanent_address]}</p>
+        <p>#{listing[:city_permanent_address]}, #{listing[:state_permanent_address]} ##{listing[:zip_permanent_address]}</p></div>
         <p class="phone-number">#{listing[:phone_numbers]}</p>
-        <a href="mailto:#{listing[:email_addresses]}">#{listing[:email_addresses]}</a>
+        <a href="mailto:#{strip_quotes(listing[:email_addresses])}">#{strip_quotes(listing[:email_addresses])}</a>
       </div>
       """
     end
